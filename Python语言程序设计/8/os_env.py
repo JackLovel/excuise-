ceae15124d7ea30b8fa855379f4aa825f45162d0
JackLovel <210172130@qq.com>
2019-01-15 22:02:11 +0800

    end

---
+++ Python语言程序设计/8/os_env.py
@@ -0,0 +1,9 @@
+import os
+
+
+# change current operation path
+os.chdir("/home")
+
+# print current path 
+current_path = os.getcwd()
+print(current_path)
---
+++ Python语言程序设计/8/os_process.py
@@ -0,0 +1,4 @@
+import os
+
+# run system command 
+os.system("dir")
