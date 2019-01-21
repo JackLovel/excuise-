a8c821fe95e9277be8986f24f0c94aa1652f7ab1
WangCC <gcc_linux@foxmail.com>
2019-01-21 22:33:42 +0800

    Update ex16.7.cpp

--- PrimerCppV5/chapter16/ex16.7.cpp
+++ PrimerCppV5/chapter16/ex16.7.cpp
@@ -3,33 +3,15 @@
 
 using namespace std;
 
-namespace ch16_5 {
-    template <typename T, unsigned size>
-    T* begin_def(T(&arr)[size])
-    {
-        return arr;
-    }
-
-    template <typename T, unsigned size>
-    T* end_def(T(&arr)[size])
-    {
-        return arr + size;
-    }
-}
-
 template<typename T, unsigned size>
-constexpr unsigned getSize(const T(&)[size])
+constexpr unsigned getSize(const T (&)[size])
 {
-    return size;
+     return size;
 }
 
 int main()
-{
-    string s[] = {"ssss", "ss", "sssszzz"};
+{    
+    string s[] = {"ssss", "aa", "ssssss", "kkkkkk"};
+
     cout << getSize(s) << endl;
-
-    char c[] = "s";
-    cout << getSize(c) << endl; // 2, 因为含有 "\0" 结束符
-
-    return 0;
 }
