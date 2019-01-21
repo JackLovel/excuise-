fd1840a106c861caef58f372e9e9d54e441f9f80
WangCC <gcc_linux@foxmail.com>
2019-01-21 20:10:32 +0800

    Update ex16.2.cpp

--- PrimerCppV5/chapter16/ex16.2.cpp
+++ PrimerCppV5/chapter16/ex16.2.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+int compare(const T &v1, const T &v2)
+{
+    if(v1 < v2) return -1;
+    if(v1 > v2) return 1;
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    cout << compare(1, 0) << endl;
+
+    vector<int> vec1{1, 2, 3}, vec2{4, 5, 6};
+    cout << compare(vec1, vec2) << endl;
+
+    return 0;
+}
 
