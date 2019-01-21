17c6385710bf0d60000a4b1b747b33f62a1ea4ec
WangCC <gcc_linux@foxmail.com>
2019-01-21 20:16:07 +0800

    Update ex16.3.cpp

--- PrimerCppV5/chapter16/ex16.3.cpp
+++ PrimerCppV5/chapter16/ex16.3.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+
+class Sales_data{};
+
+template <typename T>
+int compare(const T &v1, const T &v2)
+{
+    if(v1 < v2) return -1;
+    if(v1 > v2) return 1;
+    return 0;
+}
+
+int main(void)
+{    
+    Sales_data item1, item2;
+    cout << compare(item1, item2) << endl; 
+    // error: no match for ‘operator<’ (operand types are ‘const Sales_data’ and ‘const Sales_data’)
+    // if(v1 < v2) return -1;
+    //    ~~~^~~~
+
+    return 0;
+}
