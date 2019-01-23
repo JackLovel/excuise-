5591ee302d8ac127bca1450a8d767f9b0e1e5498
WangCC <gcc_linux@foxmail.com>
2019-01-23 17:31:58 +0800

    Update main.cpp

--- PrimerCppV5/chapter16/ex16.14/main.cpp
+++ PrimerCppV5/chapter16/ex16.14/main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <vector>
 
+#include "screen.h"
+
+int main()
+{
+    Screen<1, 5> scr('c');
+    Screen<2, 4> scr2;
+
+    std::cout << scr;
+    std::cin >> scr2;
+
+    std::cout << scr2;
+}
