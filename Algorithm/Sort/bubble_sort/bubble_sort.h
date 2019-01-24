b6fe0f6581a0793a57bf7afcb41329a02fe46376
WangCC <gcc_linux@foxmail.com>
2019-01-24 12:12:09 +0800

    Update bubble_sort.h

--- Algorithm/Sort/bubble_sort/bubble_sort.h
+++ Algorithm/Sort/bubble_sort/bubble_sort.h
@@ -1,1 +1,22 @@
+#ifndef BUBBLE_SORT_H
+#define BUBBLE_SORT_H
 
+#include <iostream>
+
+template<typename T>
+void bubble_sort(T* array, int size)
+{
+    for (int i = 0; i < size - 1; ++i)
+    {
+        for (int j = 0; j < size - 1 - i; ++j)
+        {
+            if (array[j] > array[j + 1])
+            {
+                T temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+#endif // BUBBLE_SORT_H
