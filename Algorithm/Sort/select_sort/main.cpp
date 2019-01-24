263be50d80575aa0c72bf6c3f2edd6110391f5eb
WangCC <gcc_linux@foxmail.com>
2019-01-24 13:06:40 +0800

    Update main.cpp

--- Algorithm/Sort/select_sort/main.cpp
+++ Algorithm/Sort/select_sort/main.cpp
@@ -1,1 +1,13 @@
+#include "select_sort.h"
 
+int main()
+{
+    int a[] = {3, 50, 1, 6, 7, 5, 30, 9, 8};
+    select_sort(a, 9);
+
+    for(int i = 0; i < 9; ++i)
+        std::cout << a[i] << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
