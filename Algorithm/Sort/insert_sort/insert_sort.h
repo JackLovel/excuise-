01786ba9220bfe5e12b3e40c1a227cb474b86f53
WangCC <gcc_linux@foxmail.com>
2019-01-24 13:56:54 +0800

    Update insert_sort.h

--- Algorithm/Sort/insert_sort/insert_sort.h
+++ Algorithm/Sort/insert_sort/insert_sort.h
@@ -1,1 +1,27 @@
+#ifndef INSERT_SORT_H
+#define INSERT_SORT_H
+
+#include <iostream>
+
+template<typename T>
+void insert_sort(T* array, int size)
+{
+    for (int i = 1; i < size; ++i)
+    {
+        if (array[i] < array[i - 1])
+        {
+            T temp = array[i];
+            int j = i;
+
+            while(temp < array[j - 1] && j > 0)
+            {
+                array[j] = array[j - 1];
+                --j;
+            }
+            array[j] = temp;
+        }
+    }
+}
+
+#endif // INSERT_SORT_H
 
