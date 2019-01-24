7be050f9457304a0fe9b1efb56f7f5255cd2e5f6
JackLovel <210172130@qq.com>
2019-01-24 10:36:38 +0800

    fix

--- Algorithm/search/sequence search/main.cpp
+++ Algorithm/search/sequence search/main.cpp
@@ -5,11 +5,13 @@ int main()
   int a[] = {1, 2, 3, 4, 5};
   int *p1 = normal_search(a, a + 5, 2);
 
-  if (p1 == a + 6) {
-	  std::cout << "search failded" << std::endl;
+  if (p1 == a + 6)
+  {
+    std::cout << "search failded" << std::endl;
   }
-  else{
-	std::cout << "success!" << std::endl;
+  else
+  {
+    std::cout << "success!" << std::endl;
   }
 
   return 0;
