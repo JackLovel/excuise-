f01c8ef1103be9a0627d5a1c28ef1320f0a8b8ad
WangCC <gcc_linux@foxmail.com>
2019-01-24 16:42:06 +0800

    Update main.cpp

--- Algorithm/Sort/quick_sort/main.cpp
+++ Algorithm/Sort/quick_sort/main.cpp
@@ -1,1 +1,14 @@
+#include "quick_sort.h"
+
+int main()
+{
+    int a[] = {3, 50, 1, 6, 7, 5, 30, 9, 8};
+    quick_sort(a, 9);
+
+    for(int i = 0; i < 9; ++i)
+        std::cout << a[i] << " ";
+    std::cout << std::endl;
+
+    return 0;
+}
 
