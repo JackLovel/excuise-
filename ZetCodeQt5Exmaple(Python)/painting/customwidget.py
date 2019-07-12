e2b0a56e3ba121f199c9e42e96c86c71612d627a
JackLovel <2101721230@qq.com>
2019-07-12 08:54:08 +0800

    update

--- ZetCodeQt5Exmaple(Python)/painting/customwidget.py
+++ ZetCodeQt5Exmaple(Python)/painting/customwidget.py
@@ -44,7 +44,74 @@ class BurningWidget(QWidget):
         size  = self.size()
         w = size.width()
         h = size.height()
-        
+
+        step = int(round(w / 10))
+
+        till = int(((w / OVER_CAPACITY) * self.value))
+        full = int(((w / OVER_CAPACITY) * MAX_CAPACITY))
+
+        if self.value >= MAX_CAPACITY:
+            qp.setPen(QColor(255, 255, 255))
+            qp.setBrush(QColor(255, 255, 184))
+            qp.drawRect(0, 0, full, h)
+            qp.setPen(QColor(255, 175, 175))
+            qp.setBrush(QColor(255, 175, 175))
+            qp.drawRect(full, 0, till-full, h)
+        else:
+            qp.setPen(QColor(255, 255, 255))
+            qp.setBrush(QColor(255, 255, 184))
+            qp.drawRect(0, 0, till, h)
+
+        pen = QPen(QColor(20, 20, 20), 1, Qt.SolidLine)
+
+        qp.setPen(pen)
+        qp.setBrush(Qt.NoBrush)
+        qp.drawRect(0, 0, w-1, h-1)
+
+        j = 0
+
+        for i in range(step, 10*step, step):
+            qp.drawLine(i, 0, i, 5)
+            metrics = qp.fontMetrics()
+            fw = metrics.width(str(self.num[j]))
+            qp.drawText(i-fw/2, h/2, str(self.num[j]))
+            j = j + 1
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        OVER_CAPACITY = 750
+
+        sld = QSlider(Qt.Horizontal, self)
+        sld.setFocusPolicy(Qt.NoFocus)
+        sld.setRange(1, OVER_CAPACITY)
+        sld.setValue(75)
+        sld.setGeometry(30, 40, 150, 30)
+
+        self.c = Communicate()
+        self.wid = BurningWidget()
+        self.c.updateBW[int].connect(self.wid.setValue)
+
+        sld.valueChanged[int].connect(self.changeValue)
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.wid)
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 390, 210)
+        self.setWindowTitle('Burning widget')
+        self.show()
+
+    def changeValue(self, value):
+        self.c.updateBW.emit(value)
+        self.wid.repaint()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
