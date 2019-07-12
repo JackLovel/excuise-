27108dd4f5730056f2470ed412abf0a5b18000fa
JackLovel <2101721230@qq.com>
2019-07-12 16:57:03 +0800

    add

---
+++ ZetCodeQt5Exmaple(Python)/customWidgets/customwidget.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+"""
+In this example, we create a custom widget
+"""
+
+from PyQt5.QtWidgets import (QWidgets, QSlider, QApplication, QHBoxLayout, QVBoxLayout)
+from PyQt5.QtCore import QObject, Qt, pyqtSignal
+from PyQt5.QtGui import QPainter, QFont, QColor, QPen
+import sys
+
+class Commiuncate(QObject):
+    updateBW = pyqtSignal(int)
+
+
+class BurningWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setMinimumSize(1, 30)
+        self.value = 75
+        self.num = [75, 150, 225, 300, 375, 450, 525, 600, 675]
+
+    def setValue(self, value):
+        self.value = value
+
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawWidget(qp)
+        qp.end()
+
+    def drawWidget(self, qp):
+        MAX_CAPACITY = 700
+        OVER_CAPACITY = 750
+
+        font =  QFont('Serif', 7, QFont.Light)
+        qp.setFont(font)
+
+        size = self.size()
+        w = size.width()
+        h = size.height()
+
+        step = int(round(w / 10))
+
+        till = int(((w / OVER_CAPACITY) * self.value))
+        full = int(((w / OVER_CAPACITY) * MAX_CAPACITY))
+
+        if self.value >= MAX_CAPACITY:
+            qp.setPen(QColor(255, 255, 255))
+            qp.setBrush(QColor(255, 255, 184))
+            qp.drawRect(0, 0, full, h)
+            qp.setPen(QColor(254, 175, 175))
+            qp.setBrush(QColor(255, 175, 175))
+            qp.drawRect(full, 0, till-full, h)
+        else:
+            qp.setPen(QColor(255, 255, 255))
+            qp.setBrush(QColor(255, 255, 184))
+            qp.drawRect(0, 0, till, h)
+
+         pen = QPen(QColor(20, 20, 20 ), 1, Qt.SolidLine)
+
+         qp.setPen(pen)
+         qp.setBrush(Qt.NoBrush)
+         qp.drawRect(0, 0, w-1, h-1)
+
+         j = 0
+
+         for i in range(step, 10*step, step):
+             
