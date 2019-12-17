6e54109553f9320c6da276c962ed5e1ae22f8b9d
JackLovel <gogkatsu@outlook.com>
2019-12-17 11:46:39 +0800

    update

--- rails_demo/devise_demo/config/routes.rb
+++ rails_demo/devise_demo/config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   get 'about' => 'home#about'
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+    #get 'sign_out', to: 'devise/sessions#destroy'
+  end
 end
