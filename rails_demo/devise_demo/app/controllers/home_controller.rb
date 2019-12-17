5dea285d6085b87cc00dff6dd223d17dc8fa2eac
JackLovel <gogkatsu@outlook.com>
2019-12-17 11:08:31 +0800

    update

--- rails_demo/devise_demo/app/controllers/home_controller.rb
+++ rails_demo/devise_demo/app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
+# coding: utf-8
 class HomeController < ApplicationController
+  # 只有关于页面才需要登录
+  before_action :authenticate_user!, only: [:about] 
+  
   def index
+  end
+
+  def about
   end
 end
