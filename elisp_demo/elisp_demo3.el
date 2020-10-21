5eb18ab64f70c760942ddeab2bc6d75726b617bc
WangCC <gogkatsu@outlook.com>
2020-10-21 21:09:41 +0800

    Update elisp_demo3.el

--- elisp_demo/elisp_demo3.el
+++ elisp_demo/elisp_demo3.el
@@ -1,1 +1,23 @@
+;; Elisp: Simple Emacs Lisp Examples
+;; url: http://ergoemacs.org/emacs/elisp_examples.html
+
+(defun insert-p-tag ()
+  "insert <p></p> at cursor point"
+  (interactive)
+  (insert "<p></p>")
+  (backward-char 4))
+
+(defun wrap-markup-region ()
+  "insert a markup <b></b> around a region"
+  (interactive)
+  (save-excursion
+    (goto-char (region-end))
+    (insert "</b>")
+       (goto-char (region-beginning))
+    (insert "<b>")))
+
+( transient-mark-mode 1)
+
+(defun select-current-word()
+  "select the word under cursor 
 
