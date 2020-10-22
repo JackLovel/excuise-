128198cff4060ad94dc889ac093fc7977b86ef92
JackLovel <gogkatsu@outlook.com>
2020-10-22 15:04:43 +0800

    update

--- elisp_demo/elisp_demo3.el
+++ elisp_demo/elisp_demo3.el
@@ -1,3 +1,4 @@
+(point)
 ;; Elisp: Simple Emacs Lisp Examples
 ;; url: http://ergoemacs.org/emacs/elisp_examples.html
 
@@ -14,10 +15,40 @@
     (goto-char (region-end))
     (insert "</b>")
        (goto-char (region-beginning))
-    (insert "<b>")))
+       (insert "<b>")))
 
-( transient-mark-mode 1)
+(region-beginning)(point)
+(region-end)
+(point-min)
+(goto-char 524)
+(point-max)(point)
+(forward-char 9);2344444444444444444
+(backward-char 9)
+(search-forward "cat") ;; cat 
+(search-backward "cat") ;; cat
 
-(defun select-current-word()
-  "select the word under cursor 
+(insert "sun and moon")
 
+(delete-char 9) ; 111111111111
+(delete-region 1 2)
+(delete-and-extract-region 1 2)
+
+(length "abc")
+; 提取字符
+(substring "abc" 0 2)
+(concat "some" "thing")
+(string-match "t" "cat")
+
+(match-string 1 "this is a car.")
+
+(replace-regexp-in-string "ee" "e4444444e4" "eeee33")
+
+(split-string "xy_007_cat" "_")
+
+(string-to-number "3")
+(number-to-string 3)
+(format "%d" 3)
+
+;; trim string
+(require 'subr-x)
+(string-trim " abc ")
