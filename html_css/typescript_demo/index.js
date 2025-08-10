ec9e5ad2eb34467a995bc344e252f69dbb6f9410
JackLovel <gogkatsu@outlook.com>
2025-08-10 04:53:08 -0700

    up

---
+++ html_css/typescript_demo/index.js
@@ -0,0 +1,64 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const obj = {
+    width: 10,
+    height: 15
+};
+const area = obj.width * obj.height;
+const a = 1;
+let c = "1";
+let b = 1;
+let z;
+function count(x, y) {
+    return x + y;
+}
+console.log(count(1, 2));
+console.log(typeof a);
+let person;
+person = { name: 'tom', age: 18 };
+console.log(person);
+// array
+let arr;
+// enum
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+})(Direction || (Direction = {}));
+function printStatus(data) {
+    console.log(data);
+}
+printStatus("1");
+const house = {
+    height: 100,
+    width: 100,
+    num: 3,
+    cell: 4,
+    room: '702'
+};
+console.log(house);
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    speak() {
+        console.log(`call me ${this.name} and age is ${this.age}`);
+    }
+}
+const p1 = new Person('z3', 18);
+console.log(p1);
+p1.speak();
+class Student extends Person {
+    constructor(name, age, grade) {
+        super(name, age);
+        this.grade = grade;
+    }
+    study() {
+        console.log(`${this.name} 正在努力中。。。。`);
+    }
+}
+const s1 = new Student('li', 14, '1');
+s1.study();
+//# sourceMappingURL=index.js.map
---
+++ html_css/typescript_demo/index.ts
@@ -0,0 +1,90 @@
+const obj = {
+    width: 10,
+    height: 15
+};
+const area = obj.width * obj.height;
+const a = 1;
+
+let c: string = "1";
+let b: number = 1;
+let z: any
+
+function count(x:number, y:number): number {
+    return x + y;
+}
+
+console.log(count(1,2))
+console.log(typeof a)
+
+let person: {
+    name: string,
+    age?: number
+}
+person = {name: 'tom', age: 18}
+console.log(person)
+
+// array
+let arr: string[]
+
+// enum
+enum Direction {
+    Up,
+    Down,
+    Left,
+}
+
+// type
+type Status = number | string
+
+function printStatus(data: Status): void {
+    console.log(data)
+}
+
+printStatus("1")
+
+type Area = {
+    height: number;
+    width: number;
+}
+
+type Address = {
+    num: number,
+    cell: number,
+    room: string
+}
+
+type House = Area & Address
+const house : House = {
+    height: 100,
+     width: 100,
+     num: 3,
+     cell: 4,
+     room: '702'
+}
+
+console.log(house)
+
+
+
+class Person {
+    constructor(public name: string, public age: number) { }
+    speak() {
+        console.log(`call me ${this.name} and age is ${this.age}`)
+    }
+}
+
+const p1 = new Person('z3', 18)
+console.log(p1)
+p1.speak()
+
+class Student extends Person {
+    constructor(name: string, age: number, public grade: string) {
+        super(name, age)
+    }
+    study() {
+        console.log(`${this.name} 正在努力中。。。。`)
+    }
+}
+
+const s1 = new Student('li', 14, '1')
+s1.study()
